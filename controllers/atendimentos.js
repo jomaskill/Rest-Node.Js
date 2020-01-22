06459d9c7f25d6b8f7b36191a33bfd2b8080408d
jomas <jomasjoao@gmail.com>
2020-01-22 16:47:53 -0300

    manipulação de datas

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -6,7 +6,6 @@ module.exports = app => {
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
 
-        Atendimento.adiciona(atendimento);
-        res.send('Rota de atendimentos/post');
+        Atendimento.adiciona(atendimento, res);
     });
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,14 +1,18 @@
+const moment = require('moment');
 const conexao = require('../infra/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        const atendimentoDatado = {...atendimento, dataCriacao, data};
+        const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro) console.log(erro)
-            else console.log(resultados)
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+            if(erro) res.json(erro);
+            else res.json(resultados);
         })
     }
 }
 
-module.exports = new Atendimento
+module.exports = new Atendimento;
