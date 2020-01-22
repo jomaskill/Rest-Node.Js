31964332a52af713a919dd464e04184f6330dcf0
jomas <jomasjoao@gmail.com>
2020-01-22 17:32:38 -0300

    tratamento de erros dos campos preenchids

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,15 +3,44 @@ const conexao = require('../infra/conexao')
 
 class Atendimento {
     adiciona(atendimento, res) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoDatado = {...atendimento, dataCriacao, data};
-        const sql = 'INSERT INTO Atendimentos SET ?';
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) res.json(erro);
-            else res.json(resultados);
-        })
+        /////////Tratamento dos dados//////////////////
+        //checando campos[boleanos]
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >=5
+
+        //estrutura para cada campo
+        const validacao = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                menssagem: 'data deve ser no futuro'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                menssagem: 'cliente deve posuir pelo menos 5 caracteres'
+            }
+        ]
+
+        const erros = validacao.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        //caso haja erros, e menssagem é enviada
+        if(existemErros) res.status(400).json(erros)
+        else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) res.status(400).json(erro)
+                else res.status(201.).json(resultados)
+            })
+        }
+
+        
     }
 }
 
